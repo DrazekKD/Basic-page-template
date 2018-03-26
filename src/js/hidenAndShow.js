27d8646d56034796bc12923444530ef469109009
Drazek <Drazek@gmail.com>
2018-03-26 23:19:21 +0200

    finished Accordion

--- src/js/hidenAndShow.js
+++ src/js/hidenAndShow.js
@@ -4,8 +4,61 @@ function toggleAccordion() {
     const number = this.id.toString().slice(-1);
     for(let i=1;i<=4;i++){
         sections[i].classList.remove('active');
+        switch (number){
+            case '1':{
+                sections[2].classList.remove('slim1');
+                sections[3].classList.remove('slim1');
+                sections[4].classList.remove('slim1');
+                break;
+            }
+            case '2':{
+                sections[1].classList.add('slim2');
+                sections[3].classList.add('slim2');
+                sections[4].classList.add('slim2');
+                break;
+            }
+            case '3':{
+                sections[1].classList.remove('slim3');
+                sections[2].classList.remove('slim3');
+                sections[4].classList.remove('slim3');
+                break;
+            }
+            case '4':{
+                sections[1].classList.remove('slim4');
+                sections[2].classList.remove('slim4');
+                sections[3].classList.remove('slim4');
+                break;
+            }
+        }
     }
     this.classList.add('active');
+    console.log(number===1);
+    switch (number){
+        case '1':{
+            sections[2].classList.add('slim1');
+            sections[3].classList.add('slim1');
+            sections[4].classList.add('slim1');
+            break;
+        }
+        case '2':{
+            sections[1].classList.add('slim2');
+            sections[3].classList.add('slim2');
+            sections[4].classList.add('slim2');
+            break;
+        }
+        case '3':{
+            sections[1].classList.add('slim3');
+            sections[2].classList.add('slim3');
+            sections[4].classList.add('slim3');
+            break;
+        }
+        case '4':{
+            sections[1].classList.add('slim4');
+            sections[2].classList.add('slim4');
+            sections[3].classList.add('slim4');
+            break;
+        }
+    }
 }
 
 for(let i=1;i<=4;i++){
@@ -22,8 +75,7 @@ function closeSection() {
         }
     }
 }
-
-window.onscroll = function() {closeSection(),showEffects()};
+window.onscroll = function() {closeSection(), showEffects()};
 
 
 
